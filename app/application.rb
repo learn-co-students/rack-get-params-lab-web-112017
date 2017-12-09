3c32d91168b8c814b6a5584525fadbe3097d46cc
nspp99b <chromepanthers@gmail.com>
2017-12-09 17:14:43 -0500

    added functionality for /cart and /add resources

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,14 @@
 class Application
-
+  @@cart = []
   @@items = ["Apples","Carrots","Pears"]
+
+  def self.cart
+    @@cart
+  end
+
+  def self.items
+    @@items
+  end
 
   def call(env)
     resp = Rack::Response.new
@@ -10,6 +18,23 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+    elsif req.path.match(/cart/)
+      case Application.cart.length
+      when 0
+        resp.write "Your cart is empty"
+      else
+        Application.cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
+    elsif req.path.match(/add/)
+      item_search = req.params["item"]
+       if Application.items.include?(item_search)
+         Application.cart << item_search
+         resp.write "added #{item_search}"
+       else
+         resp.write "We don't have that item"
+       end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
